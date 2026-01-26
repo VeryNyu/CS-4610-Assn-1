a7916d12bb649e6e9a04274cfc3758202b2795e5
VeryNyu <tavo_@DESKTOP-LQ9P147.localdomain>
2026-01-26 15:34:31 -0700

    seajfoiareoihopiaerigh

--- app/generations/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function GenerationPage() {
-    return (
-        <div>
-            <h1>Generation index page</h1>
-            <p>This page will display a searchable list of Pokemon generations</p>
-        </div>
-    )
-}
--- app/moves/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MovesPage() {
-    return (
-        <div>
-            <h1>Moves index page</h1>
-            <p>This page will display a searchable list of Pokemon moves</p>
-        </div>
-    )
-}
